6cf0c222d503a5b1040c6f478e7f2d4a57be532f
Lubomir Klucka <lubo.klucka@gmail.com>
2025-08-03 11:10:16 +0200

    Initial Commit

---
+++ CircularProgressBar/CircularProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularProgressBarApp.swift
+//  CircularProgressBar
+//
+//  Created by Lubo Klucka on 03/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CircularProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
